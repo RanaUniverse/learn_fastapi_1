a05205450f214cd241bc87327f95efeddbb9185e
RanaUniverse <RanaUniverse321@gmail.com>
2025-08-17 18:53:21 +0530

    normal example of fastapi

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from learn-fastapi-1!")
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None) -> dict[str, int | str | None]:
+    return {
+        "item_id": item_id,
+        "q": q,
+    }
