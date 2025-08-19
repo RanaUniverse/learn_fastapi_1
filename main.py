18fe4ec61d73a79344cfa53153f0fe8f8ee9cb82
RanaUniverse <RanaUniverse321@gmail.com>
2025-08-19 13:37:24 +0530

    added enum like things in the models/ endpoint,

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id:int):
-    return {"item_id": item_id}
+class ModelName(str, Enum):
+    alexnet = "Alexnet"
+    resnet = "REsnet"
+    lenet = "leteT"
+    planet = 234
+
+
+app = FastAPI()
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName) -> dict[str, str | ModelName]:
+
+    if model_name is ModelName.alexnet:
+        return {
+            "model_name": model_name,
+            "message": "Deep Learnig FTW!",
+        }
+
+    if model_name.value == "leteT":
+        return {
+            "model_name": model_name,
+            "message": "LeCNN all the images",
+        }
+
+    return {
+        "model_name": model_name,
+        "message": "Have some residuals",
+    }
