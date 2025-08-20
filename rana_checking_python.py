8232d79febce988683318ebb0823811b9f9ddd43
RanaUniverse <RanaUniverse321@gmail.com>
2025-08-20 13:26:32 +0530

    chekcing a demo model dump how work in pydantic

--- rana_checking_python.py
+++ rana_checking_python.py
@@ -1,8 +1,25 @@
-d = {"name": "Bob", "age": 22, "major": "Computer Science"}
-a, b, c = d.values()
+from pydantic import BaseModel
 
-print(a)  
-print(b)  
-print(c)
+class Address(BaseModel):
+    street: str
+    city: str
+    zip_code: str
 
-print(type(d.values()))
+class User(BaseModel):
+    id: int
+    name: str
+    address: Address
+
+# Create a Pydantic model instance
+user_address = Address(street="123 Main St", city="Anytown", zip_code="12345")
+user = User(id=1, name="Alice", address=user_address)
+
+# Convert the Pydantic model to a dictionary
+user_dict = user.model_dump()
+
+# print(user_dict)
+print(user_address)
+print(user)
+
+print(user_address.model_dump())
+print(type(user.model_dump()))
