eaf9132dee65e9b712f90053d43288ee656a5195
RanaUniverse <RanaUniverse321@gmail.com>
2025-08-20 16:10:07 +0530

    mixed the old get and post methods in signle module

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from enum import Enum
+
+
 from fastapi import FastAPI
 
 from pydantic import BaseModel
+
+
+from my_modules.need_extra import current_indian_time
+
+
+app = FastAPI()
 
 
 class Item(BaseModel):
@@ -10,7 +19,23 @@ class Item(BaseModel):
     tax: float | None = None
 
 
-app = FastAPI()
+class ModelName(str, Enum):
+    alexnet = "Alexnet"
+    resnet = "REsnet"
+    lenet = "leteT"
+    planet = 234
+
+
+def a_big_work():
+    for i in range(9999):
+        print(i)
+
+
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"},
+]
 
 
 @app.post("/items/")
@@ -34,4 +59,77 @@ async def create_item(
         **item.model_dump(),
         "applied_tax": applied_tax,
         "total_price": total_price,
+    }
+
+
+@app.get("/items/{item_id}")
+async def red_user_item2(item_id: str, needy: str):
+    item = {
+        "item_id": item_id,
+        "needy": "This is must need " + needy.upper(),
+    }
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int,
+    item_id: str,
+    q: str | None = None,
+    short: bool = False,
+):
+    item: dict[str, str | int] = {"item_id": item_id.upper(), "owner_id": user_id}
+    if q:
+        item.update({"q": q.upper()})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a logn description."}
+        )
+    return item
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item2(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": f"New_Value_{q}"})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description."}
+        )
+    return item
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    a_big_work()
+    return {
+        "file_path": file_path.upper(),
+        "execution_time": current_indian_time().strftime("%Y-%m-%d %H:%M:%S"),
+    }
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName) -> dict[str, str | ModelName]:
+
+    if model_name is ModelName.alexnet:
+        return {
+            "model_name": model_name,
+            "message": "Deep Learnig FTW!",
+        }
+
+    if model_name.value == "leteT":
+        return {
+            "model_name": model_name,
+            "message": "LeCNN all the images",
+        }
+
+    return {
+        "model_name": model_name,
+        "message": "Have some residuals",
     }
